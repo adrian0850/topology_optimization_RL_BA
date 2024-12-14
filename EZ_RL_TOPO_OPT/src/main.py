f00605167e4acb3cf0100853c81bb6d10531c4d4
adrian0850 <adrian@matas.de>
2024-12-14 11:39:18 +0100

    HERE IS THE MAXIMUM DEVELOPMENT ON THE PPO NB CNN VERSION

--- EZ_RL_TOPO_OPT/src/main.py
+++ EZ_RL_TOPO_OPT/src/main.py
@@ -1,12 +1,13 @@
 import tests as test
 
 NUM_ENVS = 10
+NUM_TIMESTEPS = 1e6
 
 
 def main():
     #test.env_compatability()
     #test.fem_analysis_func()
-    test.learn(NUM_ENVS)
+    test.learn(NUM_ENVS, NUM_TIMESTEPS)
     #test.loading(NUM_ENVS)
     #test.cnn()
 
